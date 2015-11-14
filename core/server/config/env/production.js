fa61010508949f34d4a2117724c85a165535acec
J. Tanner Porter <jtporter9@gmail.com>
2015-11-14 13:02:47 -0700

    second

--- core/server/config/env/production.js
+++ core/server/config/env/production.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: 'mongodb://<dbuser>:<dbpassword>@ds053894.mongolab.com:53894/jtporter9',
+    db: 'mongodb://Jtporter9:Battlefield2@ds053894.mongolab.com:53894/jtporter9',
     sessionSecret: 'productionSecret'
 };
